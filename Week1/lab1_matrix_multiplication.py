62df3c343cec478973ad4115b0e7c0ed6dfb161d
Michael Schmidlin <mschmidlin1@gmail.com>
2022-09-05 21:08:23 -0400

    dot product function is working

--- Week1/lab1_matrix_multiplication.py
+++ Week1/lab1_matrix_multiplication.py
@@ -3,6 +3,16 @@ def main():
     print("hello world")
 
 
+
+def dot(X, Y):
+    if len(X) != len(Y):
+        raise TypeError('Cannot do dot of arrays of different length.')
+    total = 0
+    for x, y in zip(X, Y):
+        total += x*y
+    return total
+
+
 def printMatrix(m): 
     for row in m:
         print(row)
--- Week1/test_lab1.py
+++ Week1/test_lab1.py
@@ -3,13 +3,56 @@ from lab1_matrix_multiplication import *
 
 class TestMatrixMult(unittest.TestCase):
 
-    def test_lab1(self):
+    def test_dot(self):
+        self.assertEqual(3454, dot((1,2,3,4,5,6), (45, 75, 46 , 67, 567, 3)))
+        self.assertEqual(3088, dot((1,2,-3,-4,5,6), (-45, 75, 46 , -67, 567, 3)))
         
+        with self.assertRaises(TypeError) as context:
+            dot([1],[2, 3])
+            self.assertTrue('Cannot do dot of arrays of different length.' in context.exception)
 
-        self.assertEqual(4, 4)
 
 
 
+
+    def test_matMult(self):
+        pass
+        # # Test1
+        # A = [[ 2, -3, 3],
+        # [-2, 6, 5],
+        # [ 4, 7, 8]]
+        # B = [[-1, 9, 1],
+        # [ 0, 6, 5],
+        # [ 3, 4, 7]]
+        # C = matrixMult(A, B)
+        # if not C == None:
+        #     printMatrix(C)
+
+
+
+        # # Test2
+        # A = [[ 2, -3, 3, 0],
+        # [-2, 6, 5, 1],
+        # [ 4, 7, 8, 2]]
+        # B = [[-1, 9, 1],
+        # [ 0, 6, 5],
+        # [ 3, 4, 7]]
+        # C = matrixMult(A, B)
+        # if not C == None:
+        #     printMatrix(C)
+
+
+
+        # # Test3
+        # A = [[ 2, -3, 3, 5],
+        # [-2, 6, 5, -2]]
+        # B = [[-1, 9, 1],
+        # [ 0, 6, 5],
+        # [ 3, 4, 7],
+        # [ 1, 2, 3]]
+        # C = matrixMult(A, B)
+        # if not C == None:
+        #     printMatrix(C)
 
 
 
