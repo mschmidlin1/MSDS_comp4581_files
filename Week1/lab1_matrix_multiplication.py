45112592d27d4ba42f9997a7466fa49032fb9711
Michael Schmidlin <mschmidlin1@gmail.com>
2022-09-05 20:36:41 -0400

    lab1 setup complete

---
+++ Week1/lab1_matrix_multiplication.py
@@ -0,0 +1,23 @@
+
+def main():
+    print("hello world")
+
+
+def printMatrix(m): 
+    for row in m:
+        print(row)
+
+def matrixMult(A, B):
+    # Your code goes here
+    
+    
+    
+    
+    pass
+
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ Week1/test_lab1.py
@@ -0,0 +1,22 @@
+import unittest
+from lab1_matrix_multiplication import *
+
+class TestMatrixMult(unittest.TestCase):
+
+    def test_lab1(self):
+        
+
+        self.assertEqual(4, 4)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
