4525132448e6adefb2c7a3f4ca55b0d1063652d1
Michael Schmidlin <mschmidlin1@gmail.com>
2022-09-14 21:10:17 -0400

    lab 2 started, insertion sort done

--- Week2/lab2_sorting.py
+++ Week2/lab2_sorting.py
@@ -8,7 +8,31 @@ def mergeSort(L):
     pass
 
 def insertionSort(L):
-    pass
+    sorted_list = []
+    for i in L:
+        if sorted_list.__len__() == 0:
+            sorted_list.append(i)
+            continue
+        inserted=False
+        for j in sorted_list:
+            if i<=j:
+                sorted_list.insert(sorted_list.index(j), i)
+                inserted=True
+                break
+        if inserted==False:
+            sorted_list.append(i)
+    return sorted_list
+
+    ## Insertion code from the video. Mine is faster even though this code is cleaner.
+
+    # for i in range(1, len(L)):
+    #     key = L[i]
+    #     j = i-1
+    #     while j>=0 and L[j]>key:
+    #         L[j+1] = L[j]
+    #         j = j-1
+    #     L[j+1] = key
+    # return L
 
 def bubbleSort(L):
     pass
--- Week2/test_sorting.py
+++ Week2/test_sorting.py
@@ -7,12 +7,19 @@ import random
 class TestMatrixMult(unittest.TestCase):
 
     def test_dot(self):
-
+        n=10
         A = [i for i in range(n)]
         random.shuffle(A)
-        self.assertEqual(1,2)
-     
+        self.assertEqual([0,1,2,3,4,5,6,7,8,9],insertionSort(A))
 
+        num_tests = 10000
+        for i in range(num_tests):
+            list_len = np.random.randint(1, 100)
+            A = [i for i in range(list_len)]
+            random.shuffle(A)
+            insertion_sorted = insertionSort(A)
+            A.sort()
+            self.assertEqual(A, insertion_sorted)
 
 
 
