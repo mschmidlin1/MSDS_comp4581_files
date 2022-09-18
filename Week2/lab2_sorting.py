14512b429cb911b6e67302a7eee331d1b4864107
Michael Schmidlin <mschmidlin1@gmail.com>
2022-09-18 10:32:11 -0400

    lab 2 done

--- Week2/lab2_sorting.py
+++ Week2/lab2_sorting.py
@@ -1,11 +1,49 @@
 
 import random
+import copy
+from time import time
+import random
+from tabulate import tabulate
 
+def merge(left, right):
+    sorted_list = []
 
+    while len(left)>0 and len(right)>0:
 
+        if left[0]<right[0]:
+            e = left.pop(0)
+            sorted_list.append(e)
+
+        else:
+            e = right.pop(0)
+            sorted_list.append(e)
+
+        
+    if len(left)>0:
+        while len(left)>0:
+            e = left.pop(0)
+            sorted_list.append(e)
+
+    
+    else:
+        while len(right)>0:
+            e = right.pop(0)
+            sorted_list.append(e)
+
+    
+    if len(right)>0 or len(left)>0:
+        raise ValueError
+
+    return sorted_list
 
 def mergeSort(L):
-    pass
+    if len(L) < 2:
+        return L[:]
+    else:
+        mid = len(L)//2
+        left = mergeSort(L[:mid])
+        right = mergeSort(L[mid:])
+        return merge(left, right)
 
 def insertionSort(L):
     sorted_list = []
@@ -35,4 +73,43 @@ def insertionSort(L):
     # return L
 
 def bubbleSort(L):
-    pass
+    A = copy.copy(L)
+    for j in range(len(A)):
+        for i in range(len(A)-1):
+            if A[i]>A[i+1]:
+                a = A[i]
+                b = A[i+1]
+                A[i] = b
+                A[i+1] = a
+
+    return A
+
+def main():
+
+    results = {}
+    results["N"] = [i for i in range(100, 1100, 100)]
+    results["Merge"] = []
+    results["Insert"] = []
+    results["Bubble"] = []
+
+    for n in results["N"]:
+        L = [i for i in range(n)]
+        random.shuffle(L)
+        t1 = time()
+        mergeSort(L)
+        t2 = time()
+        insertionSort(L)
+        t3 = time()
+        bubbleSort(L)
+        t4 = time()
+
+        results["Merge"].append((t2-t1)*1000)
+        results["Insert"].append((t3-t2)*1000)
+        results["Bubble"].append((t4-t3)*1000)
+
+    print(tabulate(results, headers=results.keys(), tablefmt='fancy_grid'))
+
+
+
+if __name__ == "__main__":
+    main()
--- Week2/test_sorting.py
+++ Week2/test_sorting.py
@@ -10,16 +10,21 @@ class TestMatrixMult(unittest.TestCase):
         n=10
         A = [i for i in range(n)]
         random.shuffle(A)
-        self.assertEqual([0,1,2,3,4,5,6,7,8,9],insertionSort(A))
-
+        self.assertEqual([0,1,2,3,4,5,6,7,8,9], insertionSort(A))
+        self.assertEqual([0,1,2,3,4,5,6,7,8,9], mergeSort(A))
+        self.assertEqual([0,1,2,3,4,5,6,7,8,9], bubbleSort(A))
         num_tests = 10000
         for i in range(num_tests):
             list_len = np.random.randint(1, 100)
             A = [i for i in range(list_len)]
             random.shuffle(A)
             insertion_sorted = insertionSort(A)
+            merge_sorted = mergeSort(A)
+            bubble_sorted = bubbleSort(A)
             A.sort()
             self.assertEqual(A, insertion_sorted)
+            self.assertEqual(A, merge_sorted)
+            self.assertEqual(A, bubble_sorted)
 
 
 
