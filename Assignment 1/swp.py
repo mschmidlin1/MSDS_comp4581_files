2180034fc9ddf9a6aa4cdba782630f5966211c0e
Michael Schmidlin <mschmidlin1@gmail.com>
2022-09-21 21:39:02 -0400

    BFS function working

--- Assignment 1/swp.py
+++ Assignment 1/swp.py
@@ -1,27 +1,61 @@
+from collections import defaultdict
+
+
 def loadGraph(edgeFilename):
     """
-    
+    Loads edges from edgeFilename.
     """
+    with open(edgeFilename) as f:
+        lines = f.readlines()
+    edges = []
+    d = defaultdict(list)
+    for line in lines:
+        verts = line.split()
+        if len(verts)!=2:
+            raise ValueError("Edge cannot have more or less than vertices.")
+        verts = int(verts[0]), int(verts[1])
+        d[verts[0]].append(verts[1])
+        d[verts[1]].append(verts[0])
+        edges.append(verts)
+    return (edges, d)
 
-    pass
-
+def unique_tuples(tuples):
+    all = []
+    for tuple in tuples:
+        all.append(tuple[0])
+        all.append(tuple[1])
+    return set(all)
 
 def BFS(G, s):
-    myqueue = MyQueue()
-
-    myqueue.enqueue(s)
-    
-    while not myqueue.empty():
-        
-
+    q = MyQueue()
+    q.enqueue(s)
+    visited = set()
+    vertex_distances = {}
+    vertex_distances[s] = 0
+    while not q.empty():
+        vertex = q.dequeue()
+        visited.add(vertex)
+        for i in G[vertex]:
+            if i not in visited and i not in q.queue:
+                q.enqueue(i)
+                vertex_distances[i] = vertex_distances[vertex] + 1
+    return sorted(vertex_distances.items())
 
 class MyQueue():
     def __init__(self):
-        queue = {}
+        self.queue = []
 
     def enqueue(self, e):
-        self.queue.add(e)
+        self.queue.append(e)
     def dequeue(self):
-        pass
+        return self.queue.pop(0)
     def empty(self):
         return not bool(len(self.queue))
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment 1/test_swp.py
@@ -0,0 +1,41 @@
+#from doctest import testfile
+import unittest
+from swp import *
+#import numpy as np
+
+class TestMatrixMult(unittest.TestCase):
+
+    def test_unique_tuples(self):
+        self.assertEqual(set([1,2,3,4,5]), unique_tuples([(1, 2), (1,3), (4, 5), (3, 2), (5, 2), (5, 1)]))
+
+    def test_queue(self):
+        Q = MyQueue()
+        Q.enqueue(1)
+        Q.enqueue(2)
+        self.assertEqual([1,2], Q.queue)
+        Q.dequeue()
+        self.assertEqual([2], Q.queue)
+        self.assertFalse(Q.empty())
+        Q.dequeue()
+        self.assertTrue(Q.empty)
+        self.assertEqual([], Q.queue)
+
+    def test_BFS(self):
+        G = {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B"]}
+        s = "A"
+        self.assertEqual([("A", 0), ("B", 1), ("C", 1)], BFS(G, s))
+
+        G = {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B", "D"], "D": ["C"]}
+        s = "A"
+        self.assertEqual([("A", 0), ("B", 1), ("C", 1), ("D", 2)], BFS(G, s))
+
+        s = "D"
+        self.assertEqual([("A", 2), ("B", 2), ("C", 1), ("D", 0)], BFS(G, s))
+
+        s = "C"
+        self.assertEqual([("A", 1), ("B", 1), ("C", 0), ("D", 1)], BFS(G, s))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
