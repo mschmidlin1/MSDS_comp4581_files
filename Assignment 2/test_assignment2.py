630ef280fcea8767a6da1729f6fa57b34b2ae340
Michael Schmidlin <mschmidlin1@gmail.com>
2022-10-03 22:23:57 -0400

    started assignment 2

---
+++ Assignment 2/test_assignment2.py
@@ -0,0 +1,12 @@
+#from doctest import testfile
+import unittest
+from schmidlin_assignment2 import *
+#import numpy as np
+
+class TestMatrixMult(unittest.TestCase):
+
+    def test_unique_tuples(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
