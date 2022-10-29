8ea6088672195a138d32fdcbb99163cafd73cb21
Michael Schmidlin <mschmidlin1@gmail.com>
2022-10-29 14:54:35 -0400

    lab 9 done

---
+++ Week9/test_lab9.py
@@ -0,0 +1,11 @@
+import unittest
+from schmidlin_lab9 import *
+
+class LAB9(unittest.TestCase):
+
+    def test_(self):
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
